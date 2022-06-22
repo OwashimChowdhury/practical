1c564a90ba824bba7404de53f203cf7b8b3cc687
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-22 14:02:59 +0530

    Add files via upload

---
+++ Details.java
@@ -0,0 +1,9 @@
+class Details
+{
+public static void main(String[]args){
+
+System.out.println("Name:OWASHIM CHOWDHURY");
+System.out.println("Department: Electrical Engineering");
+System.out.println("Course name: Java");
+}
+}
