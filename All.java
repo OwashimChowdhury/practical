3d63bf9ea9a0fda4c139356fd13b587498351355
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-22 13:44:38 +0530

    Add files via upload

---
+++ All.java
@@ -0,0 +1,17 @@
+class All{
+public static void main(String[]args){
+int a=50;
+int b=10;
+
+System.out.println("a="+a);
+System.out.println("b="+b);
+System.out.print("sum of two number is:");
+System.out.println(a+b);
+System.out.println("subtraction of two number is:");
+System.out.println(a-b);
+System.out.println("division of two number is:");
+System.out.println(a/b);
+System.out.println("multiplivcation of two number is:");
+System.out.println(a*b);
+}
+}
