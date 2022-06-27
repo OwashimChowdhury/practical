5d27348aa854024f98ca73152104717a081e727b
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 12:55:10 +0530

    Add files via upload

---
+++ Forloop.java
@@ -0,0 +1,12 @@
+
+class Forloop{  //creating Forloop class
+public static void main(String[]args) { 
+		int i=0; //int type variable i is variable name
+		
+		for (int j=1;j<=10;j++) {
+			i=i+j;   //logic
+		}
+	System.out.println("sum of 10 natural number is:");
+	System.out.println(i); //output
+}
+}
