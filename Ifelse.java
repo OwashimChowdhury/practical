e9f63ee056add788151f3dbc90dad3797f797573
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 16:40:06 +0530

    Update Ifelse.java

--- Ifelse.java
+++ Ifelse.java
@@ -1,3 +1,7 @@
+/*
+@author:Owashim chowdhury
+Java program for if else statement
+*/
 class Ifelse{  //creating Ifelse class
 public static void main(String[]args) { 
 		
@@ -5,11 +9,10 @@ int x=5;   //variable x, int type
 int y=3;
 if(x+y<10){
 System.out.println("x+y is less than 10");
-}
+}     //end of if statement
 else{
 System.out.println("x+y is greater than 10");
-}
+}    //end of else statement
 
-
-}
-}
+}     //end of main method
+}	//end of class
