da98065c2b97ee4fe4a7ed65c20efef6734a47b7
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 13:24:14 +0530

    Add files via upload

---
+++ Ifstatement.java
@@ -0,0 +1,13 @@
+class Ifstatement{  //creating Dowhile class
+public static void main(String[]args) { 
+		
+int x=5;   //variable x, int type
+int y=3;
+if(x+y<10){
+System.out.println("x+y is less than 10");
+}
+
+
+
+}
+}
