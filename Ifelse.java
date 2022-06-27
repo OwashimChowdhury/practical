8b03113527547159cf7986839335c5b4623f563d
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 13:16:36 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,15 @@
+class Dowhile{  //creating Dowhile class
+public static void main(String[]args) { 
+		
+int x=5;   //variable x, int type
+int y=3;
+if(x+y<10){
+System.out.println("x+y is less than 10");
+}
+else{
+System.out.println("x+y is greater than 10");
+}
+
+
+}
+}
