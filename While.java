fbc23b2b40225efb8de2d59528e2d2ed27061117
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 16:31:02 +0530

    Update While.java

--- While.java
+++ While.java
@@ -1,3 +1,8 @@
+/*
+@author:Owashim chowdhury
+Java program of  while loop
+*/
+
 class While{  //creating While class
 public static void main(String[]args) { 
 		int i=0; //int type variable i is variable name
@@ -5,7 +10,7 @@ public static void main(String[]args) {
 while(i<=10){   //condition
 System.out.println(i);
 i=i+2;       //logic
-}
+}  //end of while loop
 
-}
-}
+}  //end of main method
+}  // end of class
