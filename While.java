510005c337698a0529be0cd86b51da4b947f35c3
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 13:05:26 +0530

    Add files via upload

---
+++ While.java
@@ -0,0 +1,11 @@
+class Dowhile{  //creating Dowhile class
+public static void main(String[]args) { 
+		int i=0; //int type variable i is variable name
+		System.out.println("even numbers are");
+while(i<=10){   //condition
+System.out.println(i);
+i=i+2;       //logic
+}
+
+}
+}
