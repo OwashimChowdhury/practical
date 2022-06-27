d9750acae677323341c19eb0ec43f9cfd9208814
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 16:33:49 +0530

    Update Ifstatement.java

--- Ifstatement.java
+++ Ifstatement.java
@@ -1,11 +1,16 @@
+/*
+@author:Owashim chowdhury
+Java program for If statement
+*/
+
 class Ifstatement{  //creating Ifstatement class
 public static void main(String[]args) { 
 		
 int x=5;   //variable x, int type
-int y=3;
+int y=3;    //y is the variable ,int type variable
 if(x+y<10){
 System.out.println("x+y is less than 10");
-}
+}  //end of if statement
 
-}
-}
+}   //end of main method
+}   //end of class
