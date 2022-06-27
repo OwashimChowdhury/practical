ca5219d0ff42e2bd600934b735fe3c866c8320e3
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 13:47:24 +0530

    Update While.java

--- While.java
+++ While.java
@@ -1,4 +1,4 @@
-class While{  //creating Dowhile class
+class While{  //creating While class
 public static void main(String[]args) { 
 		int i=0; //int type variable i is variable name
 		System.out.println("even numbers are");
