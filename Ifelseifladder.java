6625a66a4502dfc64b13d922bb0ec094006f1c13
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-27 16:38:13 +0530

    Update Ifelseifladder.java

--- Ifelseifladder.java
+++ Ifelseifladder.java
@@ -6,17 +6,16 @@ Java program for if else if ladder
 public class Ifelseifladder {   //creating a class
 
 	public static void main(String[] args) {
-		String name="Owashim" ;  //string variable
+		String name="Owashim" ;                                              //string variable
 				if(name=="riya"){
 				System.out.println("yourname is riya");
-
-				}
+				}                                                   // end of if statement
 				else if(name=="Rony"){
 				System.out.println("yourname is Rony");
 				}
 				else{
-					System.out.println(name);   //print name
-				}   //end of else statement
+					System.out.println(name);                    //print name
+				}                                                    //end of else statement
 	}    //end of main method
 
 }       //end of class
