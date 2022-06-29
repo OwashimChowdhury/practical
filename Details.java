168bf42ac2b59d03b85f895c4cf62bf9f5a1a595
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-29 20:56:33 +0530

    Update Details.java

--- Details.java
+++ Details.java
@@ -1,3 +1,7 @@
+/*
+@author:Owashim chowdhury
+Java program to print details
+*/
 class Details
 {
 public static void main(String[]args){
@@ -5,5 +9,5 @@ public static void main(String[]args){
 System.out.println("Name:OWASHIM CHOWDHURY");
 System.out.println("Department: Electrical Engineering");
 System.out.println("Course name: Java");
-}
-}
+} //end of main method
+} //end of class
