d4a29622c93b1581599020cb540bad7d83205c7d
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-29 20:52:22 +0530

    Update AllarithmaticOperation.java

--- AllarithmaticOperation.java
+++ AllarithmaticOperation.java
@@ -1,3 +1,7 @@
+/*
+@author:Owashim chowdhury
+Java program to sum,sub,multiplication,division
+*/
 class AllarithmaticOperation{
 public static void main(String[]args){
 int a=50;
@@ -6,12 +10,12 @@ int b=10;
 System.out.println("a="+a);
 System.out.println("b="+b);
 System.out.print("sum of two number is:");
-System.out.println(a+b);
+System.out.println(a+b);  //sum of two number
 System.out.println("subtraction of two number is:");
-System.out.println(a-b);
+System.out.println(a-b);  //subtraction of two number
 System.out.println("division of two number is:");
-System.out.println(a/b);
+System.out.println(a/b);  //division of two number
 System.out.println("multiplivcation of two number is:");
-System.out.println(a*b);
-}
-}
+System.out.println(a*b);  //multiplication of two number
+} //end of main method
+} //end of class
