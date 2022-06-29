6f25033650315e30a9f3b706bd82bbb78d8533b1
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-06-29 20:46:44 +0530

    Update AreaOfSquare.java

--- AreaOfSquare.java
+++ AreaOfSquare.java
@@ -1,25 +1,24 @@
+/*
+@author:Owashim chowdhury
+Java program to calculate Area Of square
+*/
 package Owas;
 
 public class AreaOfSquare {
 	int length;
 	void put(int l) {
 		
-		length=l;
-		
+		length=l;	
 	}
 	void calculation() {
 		
-		System.out.println(length*length);
-		
-		
+		System.out.println(length*length);	//calculation or logic
 	}
-	
-
 	public static void main(String[] args) {
 		
 		AreaOfSquare s1=new AreaOfSquare();
-		s1.put(5);
-		s1.calculation();
-	}
+		s1.put(5);  //calling
+		s1.calculation(); //calling
+	} 		//end of main method
 
-}
+}		//end of class
