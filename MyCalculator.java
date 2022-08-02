1d5425e49543f7aeb64274cf6a488a6bf92dae89
OwashimChowdhury <107671048+OwashimChowdhury@users.noreply.github.com>
2022-08-02 17:59:00 +0530

    Update MyCalculator.java

--- MyCalculator.java
+++ MyCalculator.java
@@ -2,15 +2,15 @@ package exception;
 
 public class MyCalculator {
 	
-	public static  long Power(int n,int p) {
+	public static  long Power(int n,int p) {//static method
 		
-		 if (n<0 || p <0) {
+		 if (n<0 || p <0) {//if n and p is less than 0 then nthrow this exception
 			 throw new ArithmeticException("n or p should not be negative");
 		}
-		 if(n==0 && p==0) {
+		 if(n==0 && p==0) {//if both are zero then throw this
 			 throw new ArithmeticException("n or p should not be zero");
 		 }
-		 else
+		 else	//else calculate n^p
 		return (long)(Math.pow(n, p));
 	}
 
